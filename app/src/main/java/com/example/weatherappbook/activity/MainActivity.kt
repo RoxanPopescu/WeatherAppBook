484859276ea4c560362d871f89c10f8b8d72927a
roxana.popescu <Bb123456++>
2020-11-08 19:49:52 +0200

    1. Chapter 8 -  doAsync(), off the UI thread

--- app/src/main/java/com/example/weatherappbook/activity/MainActivity.kt
+++ app/src/main/java/com/example/weatherappbook/activity/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.weatherappbook.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherappbook.R
 import com.example.weatherappbook.adapters.ForecastListAdapter
+import com.example.weatherappbook.data.Request
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         "Fri 6/27 - Foggy - 21/10",
         "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18", "Sun 6/29 - Sunny - 20/7"
     )
+
     private lateinit var forecastList: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,5 +31,16 @@ class MainActivity : AppCompatActivity() {
         val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(items)
+
+        //Anko library providea a DSL to deal with asynchrony - a doAsync function
+        //that executes the code in another thread with the option to return
+        // to main tread by calling uiThread
+
+        val url = "http://api.openweathermap.org/data/2.5/forecast/daily?APPID=15646a06818f61f7b8d7823ca833e1ce&zip=94043&mode=json&units=metric&cnt=7"
+
+        doAsync{
+            Request(url).run()
+            uiThread{ longToast("Request performed")}
+        }
     }
 }
