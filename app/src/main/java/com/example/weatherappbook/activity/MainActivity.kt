5c43df8c7715f8434421bb8438f6c664c70b6765
roxana.popescu <Bb123456++>
2020-11-10 15:51:56 +0200

    1. Chapter 11 - Commit after code review! The set listadapter is missing

--- app/src/main/java/com/example/weatherappbook/activity/MainActivity.kt
+++ app/src/main/java/com/example/weatherappbook/activity/MainActivity.kt
@@ -6,23 +6,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherappbook.R
 import com.example.weatherappbook.adapters.ForecastListAdapter
-import com.example.weatherappbook.data.Request
+import com.example.weatherappbook.domain.commands.RequestForecastCommand
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.longToast
 import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
-
-    private val items = listOf(
-        "Mon 6/23 - Sunny - 31/17",
-        "Tue 6/24 - Foggy - 21/8",
-        "Wed 6/25 - Cloudy - 22/17",
-        "Thurs 6/26 - Rainy - 18/11",
-        "Fri 6/27 - Foggy - 21/10",
-        "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18", "Sun 6/29 - Sunny - 20/7"
-    )
-
-    private lateinit var forecastList: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +18,10 @@ class MainActivity : AppCompatActivity() {
 
         val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
-        val url = "http://api.openweathermap.org/data/2.5/forecast/daily?APPID=15646a06818f61f7b8d7823ca833e1ce&zip=94043&mode=json&units=metric&cnt=7"
 
         doAsync{
-            Request(url).run()
-            uiThread{ longToast("Request performed")}
+            val result =  RequestForecastCommand("94043").execute()
+            uiThread{ forecastList.adapter = ForecastListAdapter(result)}
         }
     }
 }
