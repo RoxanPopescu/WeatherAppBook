7345e094c0968cc6780f12b8bb1363c6756de295
roxana.popescu <Bb123456++>
2020-11-11 15:13:16 +0200

    1. Chapter 15 - Add kotlin extensions to activity/adapter for layouts, to reduce boiler plate code

--- app/src/main/java/com/example/weatherappbook/activity/MainActivity.kt
+++ app/src/main/java/com/example/weatherappbook/activity/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.weatherappbook.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherappbook.R
 import com.example.weatherappbook.adapters.ForecastListAdapter
 import com.example.weatherappbook.domain.commands.RequestForecastCommand
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,13 +17,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
-        forecastList.layoutManager = LinearLayoutManager(this)
+        forecast_list.layoutManager = LinearLayoutManager(this)
 
         doAsync {
             val result = RequestForecastCommand("94043").execute()
             uiThread {
-                forecastList.adapter = ForecastListAdapter(result) {
+                forecast_list.adapter = ForecastListAdapter(result) {
                     toast(it.date)
                 }
             }
